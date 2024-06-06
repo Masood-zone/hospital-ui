b7fc3e51a869672d421dac714f8b6f7563b007d4
Kirito <masoodacheampong@gmail.com>
2024-06-06 19:35:30 +0000

    chore:worked on a loader when the page loads

--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -4,7 +4,7 @@ function Loader() {
   return (
     <div className="w-screen h-screen">
       <div className="flex justify-center items-center h-full w-full">
-        <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-primary-500"></div>
+        <div className="loader"></div>
       </div>
     </div>
   );
